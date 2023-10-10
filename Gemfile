8384788d64cb086ad3aa143786a7e7162f70a5de
michigan-musicer <musicer@umich.edu>
2023-10-10 02:34:01 +0200

    Set up Minimalist Mistakes default template
    Gemfile is messed up so I just have the template in the files directly.
    Customization is to come.

--- Gemfile
+++ Gemfile
@@ -3,9 +3,12 @@
 source "https://rubygems.org"
 
 gem "jekyll"
+gem "minimal-mistakes-jekyll"
 
 group :jekyll_plugins do
-  gem 'jekyll-sitemap'
-  gem 'jekyll-feed'
-  gem 'jekyll-seo-tag'
+  gem "jekyll-paginate"
+  gem "jekyll-sitemap"
+  gem "jekyll-gist"
+  gem "jekyll-feed"
+  gem "jekyll-include-cache"
 end
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Edge Case: Nested and Mixed Lists",
+        "excerpt":"Nested and mixed lists are an interesting beast. It’s a corner case to make sure that Lists within lists do not break the ordered list numbering order Your list styles go deep enough. Ordered – Unordered – Ordered ordered item ordered item unordered unordered ordered item ordered item ordered item...","categories": ["Edge Case"],
+        "tags": ["content","css","edge case","lists","markup"],
+        "url": "/edge%20case/edge-case-nested-and-mixed-lists/",
+        "teaser": null
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["Jekyll"],
+        "tags": ["update"],
+        "url": "/jekyll/welcome-to-jekyll/",
+        "teaser": "/assets/images/500x300.png"
+      }]
